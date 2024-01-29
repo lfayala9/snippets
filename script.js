4963586a9f9a8adf4b5195ccd4591a09114f046f
Luis Ayala <lfayala9@gmail.com>
2024-01-29 17:22:10 +0100

    style: new buttons style

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-const loginButton = document.getElementById('login-button');
-const modelInput = document.getElementById('model-input')
+const loginButton = document.getElementById("login-button");
+const modelInput = document.getElementById("model-input");
+const homeButton = document.getElementById("home-button");
+const readButton = document.getElementById("read-button");
+const historyButton = document.getElementById("history-button");
+const uploadButton = document.getElementById("upload-button");
+const ppoButton = document.getElementById("ppo-button");
 
-// aquí se supone que se ejecute un script
-
-function loginFunction(){
-    return modelInput.classList.remove('hidden')
+function loginFunction() {
+  modelInput.classList.remove("hidden");
 }
 
-loginButton.addEventListener("click", loginFunction)
+function handleClick(selectedButton, otherButtons) {
+  selectedButton.classList.add("selected");
+  otherButtons.forEach((button) => button.classList.remove("selected"));
+}
+
+function homeClick() {
+  handleClick(homeButton, [readButton, historyButton, uploadButton, ppoButton]);
+}
+
+function readClick() {
+  handleClick(readButton, [homeButton, historyButton, uploadButton, ppoButton]);
+}
+
+function historyClick() {
+  handleClick(historyButton, [homeButton, readButton, uploadButton, ppoButton]);
+}
+function uploadClick() {
+  handleClick(uploadButton, [homeButton, readButton, historyButton, ppoButton]);
+}
+function ppoClick() {
+    handleClick(ppoButton, [homeButton, readButton, historyButton, uploadButton]);
+  }
+loginButton.addEventListener("click", loginFunction);
+homeButton.addEventListener("click", homeClick);
+readButton.addEventListener("click", readClick);
+historyButton.addEventListener("click", historyClick);
+uploadButton.addEventListener("click", uploadClick)
+ppoButton.addEventListener("click", ppoClick)
